7912404a5a8c317799379b0f2e6fa957c8a4b401
saurav shakya <69755787+1saura2@users.noreply.github.com>
2024-05-21 15:47:01 +0545

    Removed the persion of plroduct contents from matadataclass.cs and partialclass.cs

--- ABKS-project/Models/MetaData/PartialClass.cs
+++ ABKS-project/Models/MetaData/PartialClass.cs
@@ -10,54 +10,5 @@ namespace ABKS_project.Models.MetaData
     {
       
     }  
-/*    
-    [ModelMetadataType(typeof(CartDetailMetaData))]
-    public partial class ValidCartDetail : CartDetail
-    {
-      
-    }
 
-
-    [ModelMetadataType(typeof(OrderMetaData))]
-    public partial class ValidOrder : Order
-    {
-
-    }
-
-    [ModelMetadataType(typeof(OrderDetailMetaData))]
-    public partial class ValidOrderDetail : OrderDetail
-    {
-
-    }
-
-
-    [ModelMetadataType(typeof(OrderStatusMetaData))]
-    public partial class ValidOrderStatus : OrderStatus
-    {
-
-    }
-
-    [ModelMetadataType(typeof(ProductMetaData))]
-    public partial class ValidProduct : Product
-    {
-
-    }
-
-    [ModelMetadataType(typeof(ProductCategoryMetaData))]
-    public partial class ValidProductCategory : ProductCategory
-    {
-
-    }
-
-    [ModelMetadataType(typeof(ShoppingCartMetaData))]
-    public partial class ValidShoppingCart : ShoppingCart
-    {
-
-    }
-    
-    [ModelMetadataType(typeof(StockMetaData))]
-    public partial class ValidStock : Stock
-    {
-
-    }*/
 }
