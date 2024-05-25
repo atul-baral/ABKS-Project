5c0af26bcba47be6d253b09b3790927bb7d21ca3
saurav shakya <69755787+1saura2@users.noreply.github.com>
2024-05-25 18:17:22 +0545

    Data fetched from the Database

---
+++ ABKS-project/Areas/Admin/Controllers/OrderStatusController.cs
@@ -0,0 +1,32 @@
+﻿using ABKS_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using BCrypt.Net;
+using ABKS_project.Areas.Product.Models;
+
+namespace ABKS_project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderStatusController : Controller
+    {
+        private readonly productContext _context;
+
+        public OrderStatusController(productContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult OrderStatusFetch()
+        {
+            var OrderStatus=_context.OrderStatuses.ToList();    
+            return View(OrderStatus);
+        }
+    }
+}
---
+++ ABKS-project/Areas/Admin/Controllers/ShoppingCartController.cs
@@ -0,0 +1,33 @@
+﻿using ABKS_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using BCrypt.Net;
+using ABKS_project.Areas.Product.Models;
+
+
+namespace ABKS_project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly productContext _context;
+
+        public ShoppingCartController(productContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult ShoppingCartFetch()
+        {
+            var ShoppingCart = _context.ShoppingCarts.ToList();
+            return View(ShoppingCart);
+            
+        }
+    }
+}
