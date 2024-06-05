59eaa079aa33bec5802441f27b83e4fed2f3479c
sthakeshar <shresthakeshar291@gmail.com>
2024-06-05 13:27:15 +0545

    userid fetch not working

--- ABKS-project/Areas/Ecommerce/Controllers/HomeController.cs
+++ ABKS-project/Areas/Ecommerce/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using ABKS_project.Areas.Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ABKS_project.Areas.Ecommerce.Controllers
 {
@@ -24,6 +26,21 @@ namespace ABKS_project.Areas.Ecommerce.Controllers
             return View(product);
         }
 
+        [HttpPost]
+        public IActionResult AddToCart(int productId)
+        {
+            var userId = HttpContext.Session.GetString("UserId");
 
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Json(new { success = false, message = "User not logged in" });
+            }
+
+            // Here, you can add code to add the product to the user's cart
+            // For example, you might save the product ID and user ID to a database
+
+            return Json(new { success = true, userId });
+        }
     }
 }
+
